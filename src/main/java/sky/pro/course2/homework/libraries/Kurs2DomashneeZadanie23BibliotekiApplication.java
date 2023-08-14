4dcf16aa18001007c545860a77195037b19f3c2e
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-14 13:43:21 +0200

    2 Курс ДЗ №23 Библиотеки изначальный комментарий

---
+++ src/main/java/sky/pro/course2/homework/libraries/Kurs2DomashneeZadanie23BibliotekiApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.course2.homework.libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs2DomashneeZadanie23BibliotekiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs2DomashneeZadanie23BibliotekiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/course2/homework/libraries/Kurs2DomashneeZadanie23BibliotekiApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.course2.homework.libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs2DomashneeZadanie23BibliotekiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
