0b84fcc32ea72aecb79a6ac168158094588c17d4
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2023-08-14 14:28:51 +0200

    2 Курс ДЗ №23 Библиотеки

--- src/main/java/sky/pro/course2/homework/libraries/controller/EmployeeController.java
+++ src/main/java/sky/pro/course2/homework/libraries/controller/EmployeeController.java
@@ -20,10 +20,7 @@ public class EmployeeController {
     }
 
     @GetMapping("/add")
-    public Employee addEmployee(@RequestParam("name") String name,
-                                @RequestParam("surname") String surname,
-                                @RequestParam("departmentID") int departmentID,
-                                @RequestParam("salary") int salary) {
+    public Employee addEmployee(@RequestParam("name") String name, @RequestParam("surname") String surname, @RequestParam("departmentID") int departmentID, @RequestParam("salary") int salary) {
         return employeeService.addEmployee(name, surname, departmentID, salary);
     }
 
